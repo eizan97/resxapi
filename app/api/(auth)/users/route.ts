3d9a3886d2340baeb677b9bbfad6ccc86634aa43
izzan hafizhul <izzanhafizhul@gmail.com>
2024-05-10 23:00:54 +0700

    update DELETE user route api

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -3,7 +3,7 @@ import connect from "@/lib/db";
 import User from "@/lib/modals/user";
 import { Types } from "mongoose";
 
-const ObjectId = require('mongoose').Types.ObjectId
+const ObjectId = require("mongoose").Types.ObjectId;
 
 export const GET = async () => {
   try {
@@ -14,20 +14,17 @@ export const GET = async () => {
     return new NextResponse("Error in fetching users" + error, { status: 500 });
   }
 };
-        
-export const POST = async (request: Request) =>{
-  try{
+
+export const POST = async (request: Request) => {
+  try {
     const body = await request.json();
 
     await connect();
     const newUser = new User(body);
     await newUser.save();
 
-    return new NextResponse(
-      JSON.stringify({message: "User is created", user: newUser}),
-      {status:201}
-    );
-  }catch (error){
+    return new NextResponse(JSON.stringify({ message: "User is created", user: newUser }), { status: 201 });
+  } catch (error) {
     return new NextResponse(
       JSON.stringify({
         message: "Error in creating user",
@@ -40,40 +37,35 @@ export const POST = async (request: Request) =>{
   }
 };
 
-export const PATCH = async (request: Request) =>{
-  try{
+export const PATCH = async (request: Request) => {
+  try {
     const body = await request.json();
-    const { userId, newUserName } = body ;
+    const { userId, newUserName } = body;
 
     await connect();
 
-    if(!userId || !newUserName){
-      return new NextResponse (
-        JSON.stringify({message: "ID or new username are required"}),
-        {
-          status:400,
-        },
-      )
-    }
-
-    if(!Types.ObjectId.isValid(userId)){
-      return new NextResponse(JSON.stringify({message: "Invalid userId"}),{
-        status:400,
+    if (!userId || !newUserName) {
+      return new NextResponse(JSON.stringify({ message: "ID or new username are required" }), {
+        status: 400,
       });
     }
 
-    const updateUser = await User.findOneAndUpdate(
-      {_id: new ObjectId(userId)},
-      {username: newUserName},
-      {new: true}
-    );
-    if(!updateUser){
-      return new NextResponse(JSON.stringify({
-        message: "User not found or didn't update user successfully",
-      }),{
+    if (!Types.ObjectId.isValid(userId)) {
+      return new NextResponse(JSON.stringify({ message: "Invalid userId" }), {
         status: 400,
-      }
-    );
+      });
+    }
+
+    const updateUser = await User.findOneAndUpdate({ _id: new ObjectId(userId) }, { username: newUserName }, { new: true });
+    if (!updateUser) {
+      return new NextResponse(
+        JSON.stringify({
+          message: "User not found or didn't update user successfully",
+        }),
+        {
+          status: 400,
+        }
+      );
     }
     return new NextResponse(
       JSON.stringify({
@@ -83,6 +75,35 @@ export const PATCH = async (request: Request) =>{
       {
         status: 200,
       }
-    )
+    );
   } catch (error) {}
+};
+
+export const DELETE = async (request: Request) => {
+  try {
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get("userId");
+
+    //validte the userId
+    if (!userId) {
+      return new NextResponse(JSON.stringify({ message: "userId is required!" }), {
+        status: 400,
+      });
+    }
+
+    await connect();
+
+    const deletedUser = await User.findByIdAndDelete(new Types.ObjectId(userId));
+
+    //check id the user was found and deleted
+    if (!deletedUser) {
+      return new NextResponse(JSON.stringify({ message: "User not found" }), {
+        status: 404,
+      });
+    }
+    // Return a success response
+    return new NextResponse(JSON.stringify({ message: "User deleted successfully" }), {
+      status: 200,
+    });
+  } catch (erorr) {}
 };
